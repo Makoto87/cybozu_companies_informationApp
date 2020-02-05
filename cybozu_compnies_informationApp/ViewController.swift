8fe31dc37b0be17f3b317cc1d37b542b47548f7c
Makoto87 <makosinhori@gmail.com>
2020-02-06 00:41:56 +0900

    Initial Commit

---
+++ cybozu_compnies_informationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cybozu_compnies_informationApp
+//
+//  Created by 堀田真 on 2020/02/06.
+//  Copyright © 2020 堀田真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
