6f1213b072d0a9a1da8a140784b503f375528a86
Makoto87 <makosinhori@gmail.com>
2020-02-26 10:44:54 +0900

    design loginView

--- cybozu_compnies_informationApp/ViewController.swift
+++ cybozu_compnies_informationApp/ViewController.swift
@@ -16,11 +16,7 @@ import GoogleSignIn
 
 class ViewController: UIViewController, FUIAuthDelegate {
 
-    // テキストフィールド
-    @IBOutlet weak var emailTextFIeld: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-    
+    // Google認証ボタン
     @IBOutlet weak var googleLoginButton: UIButton!
     
     var db: Firestore!
@@ -62,6 +58,10 @@ class ViewController: UIViewController, FUIAuthDelegate {
         GIDSignIn.sharedInstance().signIn()
 //        createGoogleSigninButton()
         
+        // Googleログインボタンを丸くする
+        googleLoginButton.layer.masksToBounds = true
+        googleLoginButton.layer.cornerRadius = 25
+
     }
     
     // FBUI
@@ -113,65 +113,6 @@ class ViewController: UIViewController, FUIAuthDelegate {
         self.present(alert, animated: true)
     }
     
-    
-    // 新規作成ボタン
-    @IBAction func newAccountButton(_ sender: Any) {
-        // emailとpasswordが入っているか確認する
-        guard let email = emailTextFIeld.text, let password = passwordTextField.text else {
-            return
-        }
-        // アカできるか判断する
-        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
-            // エラーだった場合。errorは Error型。これはnilもあるのでoptional型となっている。nilが入ったらエラー
-            if let error = error {
-                print("認証失敗")
-                // アラートで通知
-                self.showErrorAlert(error: error)
-            // 成功した場合
-            } else {
-                print("新規作成成功")
-
-                // firestoreをインスタンス化
-                let db = Firestore.firestore()
-                // 生成したIDのoptional型を外す
-                guard let createId = Auth.auth().currentUser?.uid else { return }
-                // コレクションを指定して、ユーザーごとにドキュメントを作る
-                let users = db.collection("users").document("\(String(createId))")
-
-                // ドキュメントにメアドとパスワードを入れる
-                let userData: NSDictionary = ["email": email, "password": password, "userID": createId]
-                users.setData(userData as! [String : Any])
-
-                // タイムラインに遷移する
-                self.toTimeLine()
-            }
-        })
-    }
-    
-    // ログインボタン
-    @IBAction func loginButton(_ sender: Any) {
-        
-        guard let email = emailTextFIeld.text, let password = passwordTextField.text else {
-            return
-        }
-        // ログイン処理。サインインのメソッド
-        Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
-            if let error = error {
-                self.showErrorAlert(error: error)
-                print("ログイン失敗")
-            } else {
-                print("ログイン成功")
-                // firestoreをインスタンス化
-                let db = Firestore.firestore()
-                // コレクションとドキュメントを指定
-                let users = db.collection("users").document("\(String(describing: Auth.auth().currentUser?.uid))")
-                // UserDefaultにfirestoreのIdを格納
-                UserDefaults.standard.set(users.documentID, forKey: "Id")
-                self.toTimeLine()
-            }
-        })
-        
-    }
     
     // Google認証ボタン
     @IBAction func googleLoginButton(_ sender: Any) {
